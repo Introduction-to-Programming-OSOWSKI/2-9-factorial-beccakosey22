1c2d0ee0a5e54aa79bd8a8b0f9c7a41429618fd0
beccakosey22 <820512@mystma.org>
2021-10-05 09:33:56 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+#Create a function called factorial() that returns the factorial of a given variable x.
+def factorial(x):
+
+    y = 1
+
+    for i in range (1, x + 1):
+        y = y*i
+    return y
+
+print(factorial(5))
